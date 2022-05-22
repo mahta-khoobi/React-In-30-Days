37c7348d8c77ff37d9269b6944267f53c2dfaa04
khoobim <mahta.khoobi@adesso.de>
2022-05-22 19:52:51 +0200

    react elements added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,114 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import asabenehImage from './images/images.png'
+// JSX element, header
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const headerStyles = {
+  backgroundColor: '#61DBFB',
+  fontFamily: 'Helvetica Neue',
+  padding: 25,
+  lineHeight: 1.5,
+}
+const welcome = 'Welcome to React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Mahta',
+  lastName: 'Khoobi',
+}
+const date = 'May 19, 2022'
+
+
+const header = (
+  <header style={headerStyles}>
+    <div className='header-wrapper'>
+    <h1>{welcome}</h1>
+    <h2>{title}</h2>
+    <h3>{subtitle}</h3>
+    <p>Writer: {author.name}, {author.lastName}</p>
+    <small>{date}</small>
+    </div>
+  </header>
+)
+
+//main
+const mainStyles = {
+  backgroundColor: '#F3F0F5',
+}
+
+const numOne = 3
+const numTwo = 2
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+)
+
+const yearBorn = 1996
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {' '}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
+// JSX element, main
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+
+const user = (
+  <div>
+    <img src={asabenehImage} alt='asabeneh image' />
+  </div>
+)
+// JSX element, main
+const main = (
+<main style={mainStyles}>
+  <div className='main-wrapper'>
+      <p>
+        Prerequisite to get started{' '}
+        <strong>
+          <em>react.js</em>
+        </strong>
+        :
+      </p>
+      <ul>{techsFormatted}</ul>
+      {result}
+      {personAge}
+      {user}
+    </div>
+    </main>
+)
+
+const footerStyles = {
+  backgroundColor: '#61DBFB',
+}
+const copyRight = "Copyright 2022"
+// JSX element, footer
+const footer = (
+  <footer style={footerStyles}>
+    <div className='footer-wrapper'>
+      <p>{copyRight}</p>
+    </div>
+  </footer>
+)
+
+// JSX element, app, a container or a parent
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(app, rootElement)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
